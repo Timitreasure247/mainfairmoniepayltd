5330e3acc286d1545f49c05bc1a5135e3e207919
Timitreasure247 <dailyreward247@gmail.com>
2025-09-30 21:35:33 +0100

    Update JoinGroup.tsx

--- src/components/JoinGroup.tsx
+++ src/components/JoinGroup.tsx
@@ -18,7 +18,7 @@ const JoinGroup: React.FC<JoinGroupProps> = ({ isOpen, onClose, onBack }) => {
   };
 
   const handleWhatsAppJoin = () => {
-    window.open('https://chat.whatsapp.com/Lic0WEAwGei4ckaIOcDqoh?mode=ems_copy_c', '_blank');
+    window.open('https://whatsapp.com/channel/0029VbBoBwa6mYPT0P3cGw1R', '_blank');
     onClose();
   };
 
